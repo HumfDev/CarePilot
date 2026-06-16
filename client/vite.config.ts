0e546c05e0e3aa7b64f1b5e6e8f26cd45221ee82
Your Name <your.email@example.com>
2026-06-16 00:30:33 -0700

    genie added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -22,4 +22,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  define: {
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify(Date.now().toString()),
+  },
 });
